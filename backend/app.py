87b9e3838b1e31a00c4f384efde258f342e7bea2
Syed Afnan Hasan <syedafnan@Mac.fritz.box>
2026-06-07 18:18:34 +0200

    chore: Added env variable

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +12,7 @@ CORS(app)
 # =========================
 # DATABASE CONFIG
 # =========================
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://syedafnan@localhost:5432/expense_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
